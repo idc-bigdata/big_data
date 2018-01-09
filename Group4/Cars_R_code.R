e299aec9af4d6e4d4c3d8cc7c1ce4b3b771871ea
Rotem Zaarour <33748704+rotemzaarour@users.noreply.github.com>
2018-01-09 10:46:51 +0200

    Delete Cars_R_code.R

--- Group4/Cars_R_code.R
+++
@@ -1,6 +0,0 @@
-##Reading the autos.CSV files from GitHub and combine them into one Data Frame named "autos"
-autos1 <- read.csv(file = "https://github.com/idc-bigdata/big_data/raw/master/Group4/autos%20(1).csv", header = TRUE, as.is = TRUE)
-autos2 <- read.csv(file = "https://github.com/idc-bigdata/big_data/raw/master/Group4/autos%20(2).csv", header = TRUE, as.is = TRUE)
-autos3 <- read.csv(file = "https://github.com/idc-bigdata/big_data/raw/master/Group4/autos%20(3).csv", header = TRUE, as.is = TRUE)
-autos <- rbind(autos1, autos2, autos3)
-str(autos)
